f9e638f5ebd564e4e12e1b22cd3b86ba2214a5c5
Stuart Mitchell <stu@stuartmitchell.com>
2016-09-21 11:13:55 +1200

    Added second checkbox

--- src/cljs/input_bug/core.cljs
+++ src/cljs/input_bug/core.cljs
@@ -12,10 +12,17 @@
        [:div "This app shows a regression between 0.6.0-rc and 0.6.0"]
        [:div "Checkbox now does not handle transitons to nil"]
        [:div "The text next to the checkbox shows the value of :checked"]
+       [:div "both checkboxes are connected to the same atom"]
+       [:div "The below checkbox works in 0.6.0-rc (and before) not in 0.6.0"]
        [:input
         {:type      "checkbox"
-         ;:checked (some? @selections) ;; works in both
          :checked   @selections                             ;; works in 0.6.0-rc not in 0.6.0
+         :on-change (fn [] (swap! selections #(if % nil :a)))}]
+       (pr-str @selections)
+       [:div "The below checkbox works in both 0.6.0-rc and 0.6.0"]
+       [:input
+        {:type      "checkbox"
+         :checked (some? @selections) ;; works in both
          :on-change (fn [] (swap! selections #(if % nil :a)))}]
        (pr-str @selections)])))
 
